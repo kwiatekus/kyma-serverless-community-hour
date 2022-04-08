015839acb61b39c9ab39c38eddddac41663f1c1a
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2022-04-08 17:06:22 +0200

    Bug - colliding git repos

--- src/render-function-manifests.sh
+++ src/render-function-manifests.sh
@@ -5,6 +5,6 @@ echo "resources:" > ../k8s-resources/base/functions/kustomization.yaml
 for d in */ ; do
     [ -L "${d}" ] && continue
     echo "Generating k8s manifests for function ${d%/}"
-    ( cd "$d" && kyma apply function --dry-run --ci -o yaml | tail -n +2 > ../../k8s-resources/base/functions/${d%/}.yaml )
+    ( cd "$d" && kyma apply function --dry-run --ci -o yaml  > ../../k8s-resources/base/functions/${d%/}.yaml )
     echo "- ${d%/}.yaml" >> ../k8s-resources/base/functions/kustomization.yaml
 done
