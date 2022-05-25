2f41ea76fc51f3b20347762f550362d11312ddce
Kwiatosz, Krzysztof <krzysztof.kwiatosz@sap.com>
2022-05-25 18:24:02 +0200

    test commit

--- src/sanitize-fn/handler.js
+++ src/sanitize-fn/handler.js
@@ -11,7 +11,7 @@ module.exports = {
         }).catch(err => {
             console.error("Could not send event",err);
         })
-        return "OKejos Bambejos!"
+        return "OK"
     }
 }
 let sanitise = (data)=>{
